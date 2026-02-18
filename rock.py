f413af98ed2be3e0e7d099d5432044d147de4231
Derek Bauer <debauer@jackhenry.com>
2026-02-18 10:23:43 -0600

    Added to Game Logic

--- rock.py
+++ rock.py
@@ -4,6 +4,8 @@ import random
 
 
 #create some varieables
+userName = input("Enter your name: ")
+
 userInput = int(input("enter 1-Rock, 2-Paper, 3-Scissors: "))
 if userInput == 1: 
     userText = "Rock"
@@ -11,7 +13,7 @@ elif userInput == 2:
     userText = "Paper"
 else:
     userText = "Scissors"
-print (userText)
+print (f"User Selection is: {userText}")
 
 computerInput = random.randint(1,3)
 if computerInput == 1: 
@@ -20,16 +22,19 @@ elif computerInput == 2:
     computerText = "Paper"
 else:
     computerText = "Scissors"
-print (computerText)
+print (f"Computer Selection is: {computerText}")
 
 if computerInput == userInput: 
-    print ("Tie")
-else: 
-    print ("Win")
-#who wins
+    print ("The result is a Tie!")
 
 #computer = rock and user = scissors, computer wins
+if computerInput == 1 and userInput == 3:
+    print (f"Computer Wins!")
+
 #computer = rock and user = paper, user wins
+if computerInput == 1 and userInput == 2:
+    print (f"{userName} Wins!")
+    
 #computer = paper and user has scissors = computer wins 
 #computer = paper and user has rock = computer wins 
 #computer = scissors and user has paper, computer wins
